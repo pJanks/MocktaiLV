a8b7cd1d80c4fc5602693e894230c90009c740bd
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-27 11:59:48 -0700

    Incorporate router and set up imports and intial structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from '../Login/Login'
+import AllMocktails from '../AllMocktails/AllMocktails'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <header>
+        <h1>MocktaiLV</h1>
+        <Switch>
+          <Route exact path='/AllMocktails/LoggedIn' component={AllMocktails} />
+        </Switch>
       </header>
-    </div>
+      <Login />
+    </main>
   );
 }
 
