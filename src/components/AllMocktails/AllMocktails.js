f987175c198e3a573b666bedd25ff630f299cba1
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-27 15:43:37 -0700

    Add functionality to display drink cards on the screen

--- src/components/AllMocktails/AllMocktails.js
+++ src/components/AllMocktails/AllMocktails.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import MocktailCard from '../MocktailCard/MocktailCard'
 import { connect } from 'react-redux';
+import { addDrinks } from '../../actions'
+import { fetchData }  from '../../utils/helperFunctions'
 
-const AllMocktails = () => {
-  return (
-    <h2>FUCK YOU</h2>
-  )
+export class AllMocktails extends Component {
+  constructor() {
+    super()
+    this.state = {
+      allDrinks: []
+    }
+  }
+
+  componentDidMount = async () => {
+    try {
+      let allDrinksData = await fetchData('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
+      await this.props.addDrinksToStore(allDrinksData)
+      await this.setState({ allDrinks: allDrinksData })
+    }
+    catch(error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    return(
+      this.state.allDrinks.map(drink => {
+        return <MocktailCard drink={drink} />
+
+      })
+    )
+  }
 }
 
-export default AllMocktails;
+export const mapDispatchToProps = (dispatch) => ({
+  addDrinksToStore: (allDrinksData) => { dispatch(addDrinks(allDrinksData)) }
+})
+
+export default connect(null, mapDispatchToProps)(AllMocktails);
