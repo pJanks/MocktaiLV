4be592b998f597b6c11336fe81363df5d4822d46
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-27 15:45:43 -0700

    Add fetch helper function

---
+++ src/utils/helperFunctions.js
@@ -0,0 +1,9 @@
+export const fetchData = async (url, options = {}) => {
+  const response = await fetch(url, options)
+   const fetchedData = await response.json()
+   if(!response.ok) {
+     throw new Error('There was an error fetching data')
+   } else {
+     return fetchedData.drinks
+   }
+ }
