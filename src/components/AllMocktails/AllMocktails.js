196e01797d8c18f3572d48610f6c6d6a78d17227
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-27 11:58:57 -0700

    Set up component to add routes

---
+++ src/components/AllMocktails/AllMocktails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const AllMocktails = () => {
+  return (
+    <h2>FUCK YOU</h2>
+  )
+}
+
+export default AllMocktails;
