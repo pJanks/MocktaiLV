324abf193f85d27ba12880fa30e71558652f5565
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-29 08:08:59 -0700

    Adjust card for routing to details page

--- src/components/MocktailCard/MocktailCard.js
+++ src/components/MocktailCard/MocktailCard.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import DrinkDetails from '../DrinkDetails/DrinkDetails'
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addFavoriteDrink } from '../../actions'
+import { getDrink } from '../../actions'
 
-const MocktailCard = ({ drink }) => {
-  console.log(drink.drink, drink);
+
+
+
+export class MocktailCard extends Component {
+  constructor() {
+    super()
+  }
+
+handleFavoriteButtonClick = () => {
+  console.log(this.props);
+    if (!this.props.user.userVerified) {
+      window.alert('You must be logged in to do that');
+    } else {
+      console.log(this.props);
+      this.props.addToUserFavorites(this.props.drink);
+    }
+  }
+
+  handleDetailsButtonClick = (drink) => {
+    this.props.addDrinkToStore(drink)
+  }
+
+render() {
   return(
     <article>
-      <h3>{drink.strDrink}</h3>
-      <img src={drink.strDrinkThumb} alt='drink image' className='drink-image'/>
+      <h3>{this.props.drink.strDrink}</h3>
+      <button onClick={this.handleFavoriteButtonClick}><Link to={this.props.user.userVerified ? `/DrinkDetails${this.props.drink.idDrink}` : '/'}>Favorite</Link></button>
+      <button onClick={() => this.handleDetailsButtonClick(this.props.drink)} ><Link to={`/DrinkDetails${this.props.drink.idDrink}`}>Details</Link></button>
+      <img src={this.props.drink.strDrinkThumb} alt='drink image' className='drink-image'/>
     </article>
-  )
-};
+  )}
+}
 
-export default MocktailCard;
+
+export const mapDispatchToProps = (dispatch) => ({
+  addToUserFavorites: (favoriteDrink) => { dispatch(addFavoriteDrink(favoriteDrink)) },
+  addDrinkToStore: (drink) => { dispatch(getDrink(drink)) }
+})
+
+export const mapStateToProps = (state) => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MocktailCard);
