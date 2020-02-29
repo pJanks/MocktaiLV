53b400c2a0aed9772e350add5bb83659d9c18afd
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-29 08:06:21 -0700

    Add action creator for drink details page

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,13 @@ export const addDrinks = (drinks) => ({
   type: 'LOAD_NONALCOHOLIC_DRINKS',
   drinks
 })
+
+export const addFavoriteDrink = (favoriteDrink) => ({
+  type: 'ADD_FAVORITE_DRINK',
+  favoriteDrink
+})
+
+export const getDrink = (drink) => ({
+  type: 'GET_DRINK',
+  drink
+})
