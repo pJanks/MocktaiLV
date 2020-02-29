562ff1d1c40310c3b7cfa4589499c2fb0bd95d09
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-29 08:07:27 -0700

    Adjust routes for drink details

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Login from '../Login/Login'
 import AllMocktails from '../AllMocktails/AllMocktails'
+import DrinkDetails from '../DrinkDetails/DrinkDetails'
 import './App.css';
 
-function App() {
-  return (
-    <main className="App">
+export class App extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <main className="App">
       <header>
         <h1>MocktaiLV</h1>
-        <Switch>
-          <Route exact path='/AllMocktails/LoggedIn' component={AllMocktails} />
-        </Switch>
       </header>
-      <Login />
-    </main>
-  );
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <Route exact path='/AllMocktails' component={AllMocktails} />
+          <Route exact path={`/DrinkDetails${this.props.selectedDrink.idDrink}`} render={() => <DrinkDetails drink={this.props.selectedDrink} />} />
+        </Switch>
+      </main>
+    );
+  }
 }
 
-export default App;
+export const mapStateToProps = (state) => ({
+  selectedDrink: state.selectedDrink,
+})
+
+export default connect(mapStateToProps)(App);
