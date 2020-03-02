1e462a8fd71db3baf521a9caf88688c371396d94
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-03-02 09:59:37 -0700

    Adjust snapshot

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -10,37 +10,43 @@ describe('App', () => {
     instance = wrapper.instance()
   })
 
+  it('should match a snapshot as expected', () => {
+    const wrapper = shallow(<App />)
 
-
-  describe('mapStateToProps', () => {
-    it('should be able to update state', () => {
-      let mockState = {
-        allDrinks: [{
-            "strDrink": "Afterglow",
-            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
-            "idDrink": "12560"
-        },
-        {
-            "strDrink": "Alice Cocktail",
-            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
-            "idDrink": "12562"
-        }]
-      }
-
-      const expected = {
-        allDrinks: [{
-            "strDrink": "Afterglow",
-            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
-            "idDrink": "12560"
-        },
-        {
-            "strDrink": "Alice Cocktail",
-            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
-            "idDrink": "12562"
-        }]
-      }
-      const result = mapStateToProps(mockState)
-      expect(result).toEqual(expected)
-    })
+    expect(wrapper).toMatchSnapshot()
   })
+
+
+
+  // describe('mapStateToProps', () => {
+  //   it('should be able to update state', () => {
+  //     let mockState = {
+  //       allDrinks: [{
+  //           "strDrink": "Afterglow",
+  //           "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+  //           "idDrink": "12560"
+  //       },
+  //       {
+  //           "strDrink": "Alice Cocktail",
+  //           "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
+  //           "idDrink": "12562"
+  //       }]
+  //     }
+  //
+  //     const expected = {
+  //       allDrinks: [{
+  //           "strDrink": "Afterglow",
+  //           "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+  //           "idDrink": "12560"
+  //       },
+  //       {
+  //           "strDrink": "Alice Cocktail",
+  //           "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
+  //           "idDrink": "12562"
+  //       }]
+  //     }
+  //     const result = mapStateToProps(mockState)
+  //     expect(result).toEqual(expected)
+  //   })
+  // })
 })
