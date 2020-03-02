b277e715ae9f5901da97972fa058030a61e0bbdc
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-03-02 13:46:36 -0700

    Add functionality for displaying a users favorites

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import MocktailCard from '../MocktailCard/MocktailCard'
+let favorites;
+
+export const Favorites = (props) => {
+  
+  const checkLocalStorage = () => {
+    let keys = Object.keys(localStorage)
+    let user = keys.find(key => {
+      return (JSON.parse(localStorage[key]).name === props.user.name)
+    })
+    let finalUser = JSON.parse(localStorage[user])
+    let favorites = finalUser.favorites
+    return(
+      <div>
+        {!favorites.length && <h1>You have no favorites</h1>}
+        {favorites.length && favorites.map(drink => <MocktailCard drink={drink} key={drink.idDrink} />)}
+      </div>
+    )
+  }
+  return checkLocalStorage()
+}
+
+export default Favorites
