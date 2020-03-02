503ea15766ebfb1a10c2e78195846da8b39b4c76
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-03-01 18:12:56 -0700

    Add necessary tests

---
+++ src/components/AllMocktails/AllMocktails.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { AllMocktails, mapDispatchToProps } from './AllMocktails';
+import { addDrinks } from '../../actions'
+import { shallow } from 'enzyme'
+
+describe('AllMocktails', () => {
+  let wrapper, instance;
+  beforeEach(() => {
+    wrapper = shallow(<AllMocktails />)
+    instance = wrapper.instance()
+  })
+
+  it('should have a default state of an empty array', () => {
+    expect(instance.state.allDrinks.length).toEqual(0)
+  })
+
+  it('should call fetchData when component mounts', () => {
+    const fetchData = jest.fn()
+    instance.componentDidMount = jest.fn().mockImplementation(() => fetchData())
+    jest.spyOn(instance, 'componentDidMount')
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({})
+      });
+    })
+    instance.componentDidMount()
+    expect(fetchData).toHaveBeenCalled()
+    })
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with the addDrinks action when addDrinksToStore is called', () => {
+      const allDrinks =[{
+            "strDrink": "Afterglow",
+            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+            "idDrink": "12560"
+        },
+        {
+            "strDrink": "Alice Cocktail",
+            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
+            "idDrink": "12562"
+        }]
+        const mockDispatch = jest.fn();
+        const actionToDispatch = addDrinks(allDrinks)
+        const mappedProps = mapDispatchToProps(mockDispatch)
+
+        mappedProps.addDrinksToStore(allDrinks)
+
+        expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
+  })
+})
