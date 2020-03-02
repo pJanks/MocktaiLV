cde2f2eccfcbc83a281663ba3f56566d6a4cd517
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-03-01 18:13:11 -0700

    Add necessary tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,46 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { App, mapStateToProps } from './App';
+import { addDrinks } from '../../actions'
+import { shallow } from 'enzyme'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  let wrapper, instance;
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+    instance = wrapper.instance()
+  })
+
+
+
+  describe('mapStateToProps', () => {
+    it('should be able to update state', () => {
+      let mockState = {
+        allDrinks: [{
+            "strDrink": "Afterglow",
+            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+            "idDrink": "12560"
+        },
+        {
+            "strDrink": "Alice Cocktail",
+            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
+            "idDrink": "12562"
+        }]
+      }
+
+      const expected = {
+        allDrinks: [{
+            "strDrink": "Afterglow",
+            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+            "idDrink": "12560"
+        },
+        {
+            "strDrink": "Alice Cocktail",
+            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
+            "idDrink": "12562"
+        }]
+      }
+      const result = mapStateToProps(mockState)
+      expect(result).toEqual(expected)
+    })
+  })
+})
