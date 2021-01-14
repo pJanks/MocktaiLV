1a9cdc775532101c619d9e6d500e45939c8448ec
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-14 11:42:01 -0800

    Updates

---
+++ precache-manifest.5fdf434d138c2e540ebc2fc302839145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc0c2094c70b7a4378377ef0f52e226",
+    "url": "/MocktaiLV/index.html"
+  },
+  {
+    "revision": "7000e12e937e593a41d8",
+    "url": "/MocktaiLV/static/css/main.f5e876e6.chunk.css"
+  },
+  {
+    "revision": "b8fd3a6c38d1e557e97a",
+    "url": "/MocktaiLV/static/js/2.1d48f9ff.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/MocktaiLV/static/js/2.1d48f9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7000e12e937e593a41d8",
+    "url": "/MocktaiLV/static/js/main.5d6136b7.chunk.js"
+  },
+  {
+    "revision": "f60bb863c84ac5178c7e",
+    "url": "/MocktaiLV/static/js/runtime-main.d471ac05.js"
+  }
+]);
